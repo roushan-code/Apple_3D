9895c2f35a03293fb83978881ab14fac32b09dca
roushan-code <roushansingh3698@gmail.com>
2024-11-08 15:28:54 +0530

    fix //models path error

--- src/components/Iphone.jsx
+++ src/components/Iphone.jsx
@@ -12,7 +12,7 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import * as THREE from 'three';
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('../../public//models/scene.glb');
+  const { nodes, materials } = useGLTF('../../public/models/scene.glb');
 
   const texture = useTexture(props.item.img)
 
